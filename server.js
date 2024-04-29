7272e79cd0a01d53e6d3e877a31ca75707c76084
Artem <eyk6tudrdk@mail.ru>
2024-04-29 16:46:39 +0200

    Not run server

---
+++ server.js
@@ -0,0 +1,17 @@
+import app from "./app";
+import mongoose from "mongoose";
+import { DB_HOST } from process.env;
+
+mongoose.set("strictQuery", true);
+
+mongoose
+  .connect(DB_HOST)
+  .then(() => {
+    app.listen(3000);
+    console.log('Database connection successful')
+  })
+  .catch((error) => {
+    console.log(error.message);
+    process.exit(1);
+  });
+
