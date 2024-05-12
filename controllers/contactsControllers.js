52689a8b710ec57805d75b4be645be4d1ee03f90
Artem <eyk6tudrdk@mail.ru>
2024-05-12 14:03:21 +0200

    Fix try catch method and delete console log

--- controllers/contactsControllers.js
+++ controllers/contactsControllers.js
@@ -2,48 +2,76 @@ import { Contact } from "../models/contact.js";
 import { HttpError } from "../helpers/HttpError.js";
 
 export const getAllContacts = async (req, res) => {
-  const result = await Contact.find();
-  res.json(result);
+  try {
+    const { _id: owner } = req.user;
+
+    const results = await contactsService.listContacts({ owner });
+    res.json(results);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
 };
 
 export const getOneContact = async (req, res) => {
-  const { id } = req.params;
-  const result = await Contact.findById(id);
+  try {
+    const { id } = req.params;
+    const result = await Contact.findById(id);
 
-  if (!result) {
-    return res.status(404).json({ message: HttpError(404).message });
+    if (!result) {
+      return res.status(404).json({ message: HttpError(404).message });
+    }
+    res.json(result);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
   }
-  res.json(result);
 };
 
 export const deleteContact = async (req, res) => {
-  const { id } = req.params;
-  const result = await Contact.findByIdAndDelete(id);
-  if (!result) {
-    res.status(404).json({ message: HttpError(404).message });
+  try {
+    const { id } = req.params;
+    const { _id: owner } = req.user;
+    const result = await Contact.findOneAndDelete(id, owner);
+    if (!result) {
+      res.status(404).json({ message: HttpError(404).message });
+    }
+    res.json(result);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
   }
-  res.json(result);
 };
 
 export const createContact = async (req, res) => {
-  const result = await Contact.create(req.body);
-  res.status(201).json(result);
+  try {
+    const { _id: owner } = req.user;
+    const result = await Contact.create({ ...req.body, owner });
+    res.status(201).json(result);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
 };
 
 export const updateContact = async (req, res) => {
-  const { id } = req.params;
-  const result = await Contact.findByIdAndUpdate(id, req.body);
-  if (!result) {
-    res.status(404).json({ message: HttpError(404).message });
+  try {
+    const { id } = req.params;
+    const result = await Contact.findByIdAndUpdate(id, req.body);
+    if (!result) {
+      res.status(404).json({ message: HttpError(404).message });
+    }
+    res.json(result);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
   }
-  res.json(result);
 };
 
 export const updateStatusContact = async (req, res) => {
-  const { id } = req.params;
-  const result = await Contact.findByIdAndUpdate(id, req.body);
-  if (!result) {
-    res.status(404).json({ message: HttpError(404).message });
+  try {
+    const { id } = req.params;
+    const result = await Contact.findByIdAndUpdate(id, req.body);
+    if (!result) {
+      res.status(404).json({ message: HttpError(404).message });
+    }
+    res.json(result);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
   }
-  res.json(result);
 };
