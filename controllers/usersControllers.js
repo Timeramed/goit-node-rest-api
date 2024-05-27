84d94b006a8c68f07229876204e21480bf2f8929
Artem <eyk6tudrdk@mail.ru>
2024-05-27 12:48:23 +0200

    Fixed

--- controllers/usersControllers.js
+++ controllers/usersControllers.js
@@ -55,7 +55,7 @@ export const verifyEmail = async (req, res) => {
   const { verificationToken } = req.params;
   const user = await User.findOne({ verificationToken });
   if (!user) {
-    throw HttpError(401, "Email not found");
+    throw HttpError(401, "Email not found!");
   }
   await User.findByIdAndUpdate(user._id, {
     verify: true,
